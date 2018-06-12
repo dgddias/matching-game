710056832b548994cac8ad76d114d4ce3ec6d25c
Danilo Dias <danilodias@redenilf.com.br>
2018-06-12 17:22:23 -0300

    update 12-06-18

--- js/app.js
+++ js/app.js
@@ -36,3 +36,62 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+let cardOpen = [];
+
+$('.card').on('click', function () {
+
+    //let classe = $(this).children().attr('class');
+
+    // cards can be flipped
+    if (cardOpen.length < 2){
+        $(this).toggleClass("open show");
+        cardOpen.push($(this));
+    }
+    // check if cards match
+    if (cardOpen.length === 2){
+        checkOpen();
+    }
+});
+
+
+function checkOpen() {
+
+
+    if((cardOpen[0].find('i').attr('class')) === (cardOpen[1].find('i').attr('class'))){
+        cardOpen.forEach(function(e){
+           /* card.animateCss('tada', function(){*/
+                e.toggleClass("open show match");
+
+        });
+    } else {
+
+    }
+
+}
+
+
+// event handler for when the card is clicked
+function cardClick(event){
+    // check opened or matched card
+    let classes = $(this).attr("class");
+    if (classes.search('open') * classes.search('match') !== 1){
+        // both should be -1
+        return;
+    }
+    // start game if needed
+    if (!started) {
+        started = true;
+        timeCount = 0;
+        timerPtr = setTimeout(startTimer, 1000);
+    }
+    // cards can be flipped
+    if (openCards.length < 2){
+        $(this).toggleClass("open show");
+        openCards.push($(this));
+    }
+    // check if cards match
+    if (openCards.length === 2){
+        checkOpenCards();
+    }
+}
